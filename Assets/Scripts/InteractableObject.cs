644a6d54089cca1af5d29ec21012e970c12ffe7d
JaneLeroux <jane.n.leroux@icloud.com>
2026-02-05 19:56:14 -0500

    Made new class, added guzmos

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -9,4 +9,25 @@ public class InteractableObject : MonoBehaviour
     {
         onInteract?.Invoke();
     }
+
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit hitObject))
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(hitObject.point, 0.10f);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, hitObject.point);
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * 50);
+        }
+    }
 }

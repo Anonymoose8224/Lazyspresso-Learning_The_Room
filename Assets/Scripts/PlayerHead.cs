618d93b36b3f66f5e189b3bbd743b8ea1613044f
Idoindeedexist <xm045199@gmail.com>
2026-03-20 15:55:56 -0400

    Fixed the pause bug

--- Assets/Scripts/PlayerHead.cs
+++ Assets/Scripts/PlayerHead.cs
@@ -54,7 +54,10 @@ public class PlayerHead : MonoBehaviour
     {
 
         if (pauseMenu.IsPaused() || plMove == null || plCam == null || !plCam.CanLook)
+        {
+            Debug.Log("Game is not pausing!");
             return;
+        }
 
         Vector2 inputValues = ctx.ReadValue<Vector2>();
         plMove.Rotate(inputValues.x);
@@ -84,7 +87,11 @@ public class PlayerHead : MonoBehaviour
     private void TogglePause(InputAction.CallbackContext ctx)
     {
         if (pauseMenu != null)
+        {
             pauseMenu.TogglePause();
+            Debug.Log("Game is toggling paused!");
+
+        }
     }
 
     private void OnDisable()

baa1a8ca37216121abf47baa4e6336ffdf5fbb6b
RuthieGG <ruthggarcia99@gmail.com>
2026-04-21 16:56:10 -0400

    adding the interaction for dialogue, not done yet

--- Assets/Scripts/NPCInteraction.cs
+++ Assets/Scripts/NPCInteraction.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class NPCInteraction : Baseinteractable
 {
-    [SerializeField] PuzzleSolving PzSolve; 
+    [SerializeField] PuzzleSolving PzSolve;
+    [SerializeField] DialogueManager dialogueManager;
+    [SerializeField] InventorySystem inventorySystem;
+
+    [SerializeField] DialogueSystem hasItemDialogue;
+    [SerializeField] DialogueSystem NoItemDialogue;
+
+    [SerializeField] string requiredItem = "";
+
     public override void Interact(Ray ray, float maxDistance)
     {
-
-        PzSolve.PuzzleSystem();
+        //if(inventorySystem
+       // {
+          //  dialogueManager.StartDialogue(hasItemDialogue);
+            PzSolve.PuzzleSystem();
+        //}
+        //else
+        //{
+       //     dialogueManager.StartDialogue(NoItemDialogue);
+       // }
+        
     }
 
 }

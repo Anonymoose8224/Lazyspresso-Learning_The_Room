e31aafd2203ce25cf0e9a5572277bbe970bb4bed
Idoindeedexist <xm045199@gmail.com>
2026-04-30 10:24:18 -0400

    Adding sounds

--- Assets/Scripts/NPCInteraction.cs
+++ Assets/Scripts/NPCInteraction.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class NPCInteraction : Baseinteractable
 {
     [SerializeField] PuzzleSolving PzSolve;
+    [SerializeField] private AudioPlayer AudClip;
+    [SerializeField] private int IndexSound;
     public override void Interact(Ray ray, float maxDistance)
     {
+        AudClip.PlaySound(IndexSound);
         PzSolve.PuzzleSystem();
     }
 

c2f13ffdbbbc5f8695b0cc73aaca6ea196a38af0
Idoindeedexist <xm045199@gmail.com>
2026-05-01 11:44:44 -0400

    Add menu song and menu screen

--- Assets/Main Menu assets/Scripts/MainMenu.cs
+++ Assets/Main Menu assets/Scripts/MainMenu.cs
@@ -3,8 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] AudioPlayer  MusCubeMenu;
+
+    private void Awake()
+    {
+        MusCubeMenu.PlayMusic();
+    }
     public void StartGame()
     {
+        MusCubeMenu.PauseMusic();
         SceneManager.LoadScene("GameScene"); // name of your game scene
     }
 
